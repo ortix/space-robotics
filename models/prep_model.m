53f8c72b4f3e156a1bc1d0a1b8a49b3ebb84f45d
Ortix <nicktsutsunava@gmail.com>
2017-04-05 11:41:08 +0200

    model prepper as a function

--- models/prep_model.m
+++ models/prep_model.m
@@ -1,9 +1,9 @@
+function prep_model(model)
 % Prepares your to correctly use and display the visualizer. First
 % copy the contents of visualizer.slx into your model/subsystem and
 % then run this script. Don't forget to the the model variable!
 
 % Enter the name of your model here
-model = 'my_model';
 
 load_system(model);
 set_param(model,'PostLoadFcn','cd(fileparts(which(bdroot)))')
