8792f2d63e5c84f2d81df51d14d746a46ace0d6a
Mkooper <martin.kooper@gmail.com>
2017-04-13 09:59:47 +0200

    Now checks for minimum radius and shifts points accordingly

--- libraries/linTrajSpline.m
+++ libraries/linTrajSpline.m
@@ -1,4 +1,4 @@
-function ptsOut = linTraj(pointsIn,sr,vMax,ease)
+function ptsOut = linTrajSpline(pointsIn,sr,vMax,ease)
 % Takes points in a 3xn vector containing all points [x y z]' the
 % robot's EEF should pass through and generates linearly
 % interpolated paths through them, based on steps/s (sr) and max
@@ -10,23 +10,20 @@ if ease > 0.3
     disp('Ease too big. Set to 0.3');
     ease = 0.3;
 elseif ease <0
-    ease = 0
+    ease = 0;
     disp('Ease too small. Set to 0');
 end
 
-% if length(pointsIn) < 2
-%     disp('Take current position');
-%     pointsIn = [getPos pointsIn];
-% end
 
-
+% Minimum radius sphere through which the EEF cannot move.
+Rmin = 0.5;
 
 
 nPts = length(pointsIn);
 
+
 % Declare memory. I do this dynamically since we dont know how many
 % steps will be generated.
-
 ptsLin = [];
 ptsEased = [];
 travelTime = zeros(1,nPts);
@@ -35,7 +32,7 @@ travelTime = zeros(1,nPts);
 xyzDist = diff(pointsIn,1,2);
 travDist = sqrt(  sum(  xyzDist(:,:).^2));
 
-% S curve interpolate for each set of two points.
+% Interpolation for each set of two points.
 for i = 1:nPts-1
     
     % Create spline through current and next point
@@ -44,21 +41,50 @@ for i = 1:nPts-1
     
     % Determine #segments based on path length, sr and max velocity.
     % Try to correct for S curve easing that increases max velocity later.
-    segs = round(sr*travDist(i)/vMax); % /diff(ease));
+    segs = round(sr*travDist(i)/vMax/(1-2*ease)); 
     
     % Linearly interpolate between current and next point.
-    steps = [linspace(pointsIn(1,i),pointsIn(1,i+1),segs)
-            linspace(pointsIn(2,i),pointsIn(2,i+1),segs)
-            linspace(pointsIn(3,i),pointsIn(3,i+1),segs)];
+    steps = zeros(3,segs);
+    for j = 1:3
+        steps(j,:) = linspace(pointsIn(j,i),pointsIn(j,i+1),segs);
+    end
    
+        
+    %%%%%%%%% The following can maybe be replaced by our own polynomial
+    %%%%%%%%% smoothstepping 
+    
+    % Find the other end of the spline description and interpolate
+    % over the spline. This eliminates descending vectors and works
+    % for all directions.
     crvEnd = curve.breaks(end);
     xq = linspace(0,crvEnd,segs);
     y = smf2(xq ,[ease*crvEnd (1-ease)*crvEnd])*crvEnd;
     
-    % This gives an S interpolation result
+    % Evaluate the spline from an S distribution of points.
     posSmf2 = fnval(curve,y);
+    %%%%%%%%%
     
-    % Calculate travel time based on #segments and sr
+    % Now check whether any of the points lie inside a minimum
+    % radius.
+    for h = 1:segs
+        
+        % Get vector norm from 0..point.
+        ptLin = norm(steps(:,h));
+        ptSMF = norm(posSmf2(:,h));
+        
+        % If the vector is smaller than Rmin, scale it to outside
+        % the minimum radius, for both linear and smf points.
+        if ptLin < Rmin
+            steps(:,h) = steps(:,h)./ptLin .* Rmin;
+        end
+        
+        if ptSMF < Rmin
+            posSmf2(:,h) = posSmf2(:,h)./ptSMF .* Rmin;
+        end
+        
+    end
+    
+    % Calculate travel time based on #segments and sr.
     travelTime = [travelTime segs/sr];
     
     % Output eased and linear points.
@@ -84,4 +110,5 @@ plot3(pointsIn(1,:),pointsIn(2,:),pointsIn(3,:),'-o','LineWidth',1);
 plot3(ptsEased(1,:),ptsEased(2,:),ptsEased(3,:),'go','LineWidth',2);
 plot3(ptsLin(1,:),ptsLin(2,:),ptsLin(3,:),'bo','LineWidth',1);
 cameratoolbar
+shg
 end
